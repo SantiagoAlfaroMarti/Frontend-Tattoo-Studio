deff07d22959ce7779e05825b27d5f14b57c4739
unknown <santiagoalfamar@gmail.com>
2024-07-24 23:26:44 +0200

    feat: create showMyAppointments, modifyUsersProfile and getAllUsers

--- src/services/apiCall.js
+++ src/services/apiCall.js
@@ -1,7 +1,12 @@
 const URL = 'http://localhost:4000/api';
 
-export const registerUser = async (credentials) => {
-    const request = await fetch(`${URL}/register`, {
+export const getAllServices = async () => {
+    const response = await fetch(`${URL}/api/services`);
+    return await response.json();
+};
+
+export const register = async (credentials) => {
+    const request = await fetch(`${URL}/api/auth/register`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -11,8 +16,8 @@ export const registerUser = async (credentials) => {
     const result = await request.json();
     return result;
 }
-export const loginUser = async (credentials) => {
-    const request = await fetch(`${URL}/login`, {
+export const userLogin = async (credentials) => {
+    const request = await fetch(`${URL}/api/auth/login`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -23,7 +28,7 @@ export const loginUser = async (credentials) => {
     return result;
 }
 export const getUserProfile = async (token) => {
-    const response = await fetch(`${URL}/users/profile`, {
+    const response = await fetch(`${URL}/api//users/profile`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
@@ -32,3 +37,39 @@ export const getUserProfile = async (token) => {
     });
     return await response.json()
 }
+export const showMyAppointments = async (token) => {
+    const response = await fetch(`${URL}/api/appointments/scheduled`, {
+        method: "GET",
+        headers: {
+            "content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    return await response.json();
+};
+
+export const modifyUserProfile = async (changes, token) => {
+    const response = await fetch(`${URL}/api/users/profile`, {
+        method: "PUT",
+        headers: {
+            "content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(changes),
+    });
+
+    return await response.json();
+};
+
+export const getAllUsers = async (token) => {
+    const response = await fetch(`${URL}/api/users`, {
+        method: "GET",
+        headers: {
+            "content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    return await response.json();
+};

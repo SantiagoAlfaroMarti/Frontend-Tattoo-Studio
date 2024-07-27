9b28a9c46056eff1747a33b8d2c1b5733daa3d74
unknown <santiagoalfamar@gmail.com>
2024-07-28 00:19:42 +0200

    feat: update Header.jsx const passport, const token and const role

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,15 @@ export const Header = () => {
 
     const navigate = useNavigate();
 
+    const passport = JSON.parse(localStorage.getItem('passport'));
+    const token = passport ? passport.token : null;
+    const role = passport ? passport.tokenData.role_id : null;
+
+    const handleLogout = () => {
+        localStorage.removeItem('passport');
+         navigate('/login');
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
             <div className="container">

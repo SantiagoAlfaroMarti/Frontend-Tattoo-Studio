7b4a377f43ab1e81194fc1e204723441b89034e9
unknown <santiagoalfamar@gmail.com>
2024-07-28 16:36:04 +0200

    update: update const navigate Admin.jsx

--- src/components/utils/function.js
+++ src/components/utils/function.js
@@ -9,7 +9,6 @@ export const isTokenValid = (expireAt) => {
         const time = new Date();
         const currentTimeStamp = Date.parse(time) / 1000;
         if (currentTimeStamp > expireAt) {
-            console.log("Hemos entrado en el if");
             localStorage.removeItem("Passport");
             clearInterval(interval)
         }
--- src/views/Admin/Admin.jsx
+++ src/views/Admin/Admin.jsx
@@ -1,38 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { deleteUserById, getAllUsers } from '../../services/apiCall.js';
 import './Admin.css';
 
 export const Admin = () => {
     const [users, setUsers] = useState([])
-
+    const navigate = useNavigate();
     const passport = JSON.parse(localStorage.getItem("passport"))
-    const token = passport.token
+    const token = passport ? passport.token : null;
 
     useEffect(() => {
         const bringAllUsers = async () => {
             const allUsers = await getAllUsers(token)
             console.log(allUsers)
-            if (allUsers.success){
+            if (allUsers.success) {
                 setUsers(allUsers.data)
+            } else {
+                navigate('/login');
             }
         };
         bringAllUsers()
-    }, []);
+    }, [navigate, token]);
 
     const deleteUserHandler = async (e) => {
         const id = +e.target.name;
-    const res = await deleteUserById(token, id);
-    if (res.success) {
-      const remainingUsers = users.filter((user) => {
-        if (user.id !== id) {
-            console.log(user)
-            return user
-        };
-      });
-      console.log(remainingUsers)
-      setUsers(remainingUsers);
-    }
-  };
+        const res = await deleteUserById(token, id);
+        if (res.success) {
+            const remainingUsers = users.filter((user) => {
+                if (user.id !== id) {
+                    console.log(user)
+                    return user
+                };
+            });
+            console.log(remainingUsers)
+            setUsers(remainingUsers);
+        }
+    };
     return (
         <>
             <h1>Admin</h1>
@@ -49,7 +52,12 @@ export const Admin = () => {
                             <div className="content">{user.id}</div>
                             <div className="content">{user.email}</div>
                             <div className="content">{user.created_at}</div>
-                            <div className="content"><input type="button" name={user.id} value="🛇" onClick={deleteUserHandler}/></div>
+                            <div className="content"><input
+                                type="button"
+                                name={user.id}
+                                value="🛇"
+                                onClick={deleteUserHandler}
+                            /></div>
                         </div>
                     )
                 })}
